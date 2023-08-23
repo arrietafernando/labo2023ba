c38231087c241cd3b7c0344ba2a2c1de984d8086
Fernando Arrieta <arrietafernando@gmail.com>
2023-08-23 17:21:53 -0300

    delete setups

--- src/rpart/setup-gcp.R
+++
@@ -1,8 +0,0 @@
-
-################################################################################
-LABO_WD = "/Users/fernando/Dropbox/MBA/UA - Catedra Labo I/labo_github"
-LABO_PROJ_WD = paste0(LABO_WD, "/R_Projects/Clase 1")
-LABO_DATA_WD = paste0(LABO_WD, "/datasets")
-LABO_EXP_WD = paste0(LABO_WD, "/exp")
-################################################################################
-
