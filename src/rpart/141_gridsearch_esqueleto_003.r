97c84d94eaa95d16c3ba7c5f5d59d167896e768e
Fernando Arrieta <arrietafernando@gmail.com>
2023-08-24 18:41:56 -0300

    2023.08.24 18:41

--- src/rpart/141_gridsearch_esqueleto_003.r
+++ src/rpart/141_gridsearch_esqueleto_003.r
@@ -42,13 +42,13 @@ PARAM$semillas <- MIS_SEMILLAS
 
 particionar <- function(data, division, agrupa = "", campo = "fold", start = 1, seed = NA) {
   if (!is.na(seed)) set.seed(seed)
-
+  
   bloque <- unlist(mapply(function(x, y) {
     rep(y, x)
   }, division, seq(from = start, length.out = length(division))))
-
+  
   data[, (campo) := sample(rep(bloque, ceiling(.N / length(bloque))))[1:.N],
-    by = agrupa
+       by = agrupa
   ]
 }
 #------------------------------------------------------------------------------
@@ -56,38 +56,38 @@ particionar <- function(data, division, agrupa = "", campo = "fold", start = 1, 
 ArbolEstimarGanancia <- function(semilla, param_basicos) {
   # particiono estratificadamente el dataset
   particionar(dataset, division = c(7, 3), agrupa = "clase_ternaria", seed = semilla)
-
+  
   # genero el modelo
   # predecir clase_ternaria a partir del resto
   modelo <- rpart("clase_ternaria ~ .",
-    data = dataset[fold == 1], # fold==1  es training,  el 70% de los datos
-    xval = 0,
-    control = param_basicos
+                  data = dataset[fold == 1], # fold==1  es training,  el 70% de los datos
+                  xval = 0,
+                  control = param_basicos
   ) # aqui van los parametros del arbol
-
+  
   # aplico el modelo a los datos de testing
   prediccion <- predict(modelo, # el modelo que genere recien
-    dataset[fold == 2], # fold==2  es testing, el 30% de los datos
-    type = "prob"
+                        dataset[fold == 2], # fold==2  es testing, el 30% de los datos
+                        type = "prob"
   ) # type= "prob"  es que devuelva la probabilidad
-
+  
   # prediccion es una matriz con TRES columnas,
   #  llamadas "BAJA+1", "BAJA+2"  y "CONTINUA"
   # cada columna es el vector de probabilidades
-
-
+  
+  
   # calculo la ganancia en testing  qu es fold==2
   ganancia_test <- dataset[
     fold == 2,
     sum(ifelse(prediccion[, "BAJA+2"] > 0.025,
-      ifelse(clase_ternaria == "BAJA+2", 117000, -3000),
-      0
+               ifelse(clase_ternaria == "BAJA+2", 117000, -3000),
+               0
     ))
   ]
-
+  
   # escalo la ganancia como si fuera todo el dataset
   ganancia_test_normalizada <- ganancia_test / 0.3
-
+  
   return(ganancia_test_normalizada)
 }
 #------------------------------------------------------------------------------
@@ -96,14 +96,14 @@ ArbolesMontecarlo <- function(semillas, param_basicos) {
   # la funcion mcmapply  llama a la funcion ArbolEstimarGanancia
   #  tantas veces como valores tenga el vector  ksemillas
   ganancias <- mcmapply(ArbolEstimarGanancia,
-    semillas, # paso el vector de semillas
-    MoreArgs = list(param_basicos), # aqui paso el segundo parametro
-    SIMPLIFY = FALSE,
-    mc.cores = 1
+                        semillas, # paso el vector de semillas
+                        MoreArgs = list(param_basicos), # aqui paso el segundo parametro
+                        SIMPLIFY = FALSE,
+                        mc.cores = 1
   ) # se puede subir a 5 si posee Linux o Mac OS
-
+  
   ganancia_promedio <- mean(unlist(ganancias))
-
+  
   return(ganancia_promedio)
 }
 #------------------------------------------------------------------------------
@@ -139,7 +139,7 @@ cat(
   sep = "",
   "max_depth", "\t",
   "min_split", "\t",
-  "minbucket", "\t",
+  "min_bucket", "\t",
   "cp", "\t",
   "ganancia_promedio", "\n"
 )
@@ -159,9 +159,9 @@ for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
             "minsplit: ", vmin_split, "\t",
             "minbucket: ", vmin_bucket, "\t",
             "cp: ", vcp, "\n"
-            )
+        )
         
-        if(vmin_bucket == 1) { next }
+        if(vmin_bucket < 2) { next }
         
         # vminsplit  minima cantidad de registros en un nodo para hacer el split
         param_basicos <- list(
@@ -170,7 +170,7 @@ for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
           "minbucket" = vmin_bucket, # minima cantidad de registros en una hoja
           "maxdepth" = vmax_depth
         ) # profundidad máxima del arbol
-    
+        
         # Un solo llamado, con la semilla 17
         #ganancia_promedio <- ArbolesMontecarlo(ksemillas, param_basicos)
         ganancia_promedio <- ArbolesMontecarlo(PARAM$semillas, param_basicos)
@@ -192,40 +192,3 @@ for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
 
 tictoc::toc()
 
-###
-
-library(doFuture)
-registerDoFuture()
-plan(multisession)
-
-y <- foreach(x = 1:4, y = 1:10) %dopar% {
-  z <- x + y
-  sqrt(z)
-}
-y
-###
-
-library(doFuture)
-plan(multisession)
-
-cutoff <- 0.10
-y <- foreach(x = mtcars, .export = c("cutoff")) %dofuture% {
-  mean(x, trim = cutoff)
-}
-names(y) <- colnames(mtcars)
-y
-
-###
-library (tidyr)
-
-params <- data.frame(vmax_depth  = c(4, 6, 8, 10, 12, 14),
-                     vmin_split  = c(1000, 800, 600, 400, 200, 100, 50, 20, 10),
-                     vcp         = c(1, 0.5, 0.1, 0.05, 0.01, 0.005)
-                    )
-                     #vmin_bucket = c(floor(vmin_split/2), floor(vmin_split/4), floor(vmin_split/6), floor(vmin_split/8)),
-                     #
-
-df <- params %>%
-  split(.$Parameter) %>%              # create a dataframe of values for each parameter
-  lapply(function(df){df$Value}) %>%  # extract the values for each parameter as an array
-  expand.grid()                       # generate all combinations
