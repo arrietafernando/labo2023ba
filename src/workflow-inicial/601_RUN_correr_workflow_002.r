b95f2d9c34df371e13a71b2009bd860562308967
Fernando Arrieta <arrietafernando@gmail.com>
2023-09-08 18:05:13 -0300

    2023.09.08 18:05

--- src/workflow-inicial/601_RUN_correr_workflow_002.r
+++ src/workflow-inicial/601_RUN_correr_workflow_002.r
@@ -16,9 +16,9 @@ PROJ_HOME = "~/labo2023ba"
 #/Users/fernando/Dropbox/MBA/UA - Catedra Labo I/labo_github/github/labo2023ba_RStudio/src/workflow-inicial/611_CA_reparar_dataset_001.r
 source(paste0(PROJ_HOME, "/src/workflow-inicial/611_CA_reparar_dataset_001.r"))
 source(paste0(PROJ_HOME, "/src/workflow-inicial/621_DR_corregir_drifting_001.r"))
-source(paste0(PROJ_HOME, "/src/workflow-inicial/631_FE_historia_001.r"))
-source(paste0(PROJ_HOME, "/src/workflow-inicial/641_TS_training_strategy_001.r"))
+source(paste0(PROJ_HOME, "/src/workflow-inicial/631_FE_historia_002.r"))
+source(paste0(PROJ_HOME, "/src/workflow-inicial/641_TS_training_strategy_002.r"))
 
 # ultimos pasos, muy lentos
-source(paste0(PROJ_HOME, "/src/workflow-inicial/651_HT_lightgbm_001.r"))
-source(paste0(PROJ_HOME, "/src/workflow-inicial/661_ZZ_final_001.r"))
+source(paste0(PROJ_HOME, "/src/workflow-inicial/651_HT_lightgbm_002.r"))
+source(paste0(PROJ_HOME, "/src/workflow-inicial/661_ZZ_final_002.r"))
